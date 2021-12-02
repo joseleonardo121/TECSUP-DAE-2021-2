fc8f9447e2c556e07acfa18bf0bfae975bd6ea6d
joseleonardo121 <63433595+joseleonardo121@users.noreply.github.com>
2021-12-02 00:47:01 -0500

    Add files via upload

---
+++ jose_gonzales_peñares-TECSUP-DAE-2021-2-B-lab13/lab13/src/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class Contact extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Contacto</h1>
+            </div>
+        )
+    }
+}
+export default Contact;
---
+++ jose_gonzales_peñares-TECSUP-DAE-2021-2-B-lab13/lab13/src/notfound.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const Notfound = () => 
+<h1>Pagina no encontrada</h1>
+
+export default Notfound;
